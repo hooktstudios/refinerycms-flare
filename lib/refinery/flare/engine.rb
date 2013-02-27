9cc34a929e91a3cf7a112603abfb1889e2b0e187
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-02-26 21:18:24 -0500

    Force decorators load

--- lib/refinery/flare/engine.rb
+++ lib/refinery/flare/engine.rb
@@ -10,11 +10,16 @@ module Refinery
         Refinery::Plugin.register do |plugin|
           plugin.name = "flare"
           plugin.hide_from_menu = true
+          plugin.pathname = root
         end
       end
 
       config.after_initialize do
         Refinery.register_extension(Refinery::Flare)
+        # @todo Find out why controller decorators aren't loaded at first
+        Dir[root.join('app', 'decorators', '**', '*_decorator.rb')].flatten.uniq.each do |decorator|
+          Rails.application.config.cache_classes ? require(decorator) : load(decorator)
+        end
       end
     end
   end
